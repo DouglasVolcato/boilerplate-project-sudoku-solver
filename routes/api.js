046c11e06116f938c105648674830935ece441ed
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-29 23:50:16 -0300

    chore: implement error responses

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,11 @@ module.exports = function (app) {
       return res.json({ error: "Required field(s) missing" });
     }
 
-    if (coord.length > 2 || coord[0].toLowerCase().charCodeAt(0) - 97 + 1 > 9) {
+    if (
+      coord.length !== 2 ||
+      coord[0].toLowerCase().charCodeAt(0) - 97 + 1 > 9 ||
+      coord[1] == 0
+    ) {
       return res.json({ error: "Invalid coordinate" });
     }
 
